89626051fd6f72ffaa25fc848fed5e1e87a7b62b
Eirik S. Morland <eirik@morland.no>
2025-10-12 20:34:06 +0200

    Now we have 2 samples and a test for samples

---
+++ cypress/e2e/sample.cy.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+describe('FESK real time with known samples', () => {
+
+  it('should decode audio from sample 1', () => {
+    cy.visit('/')
+
+    cy.get('.debug-panel summary', { timeout: 10000 }).click()
+    cy.get('#sample1Btn').click()
+    cy.wait(4000)
+    cy.get('#out .decoded-ok', { timeout: 20000 }).should('not.be.empty')
+     .should('contain.text', 'test')
+  })
+
+  it('should decode audio from sample 2', () => {
+    cy.visit('/')
+
+    cy.get('.debug-panel summary', { timeout: 10000 }).click()
+    cy.get('#sample2Btn').click()
+    cy.wait(20000)
+    cy.get('#out .decoded-ok', { timeout: 20000 }).should('not.be.empty')
+     .should('contain.text', 'abc 123')
+  })
+})
