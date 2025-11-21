22775042abb5bd66f9f1eee8cbd6c011fa90f47b
Eirik S. Morland <eirik@morland.no>
2025-11-21 17:56:59 +0100

    Simplify test

--- cypress/e2e/sample.cy.js
+++ cypress/e2e/sample.cy.js
@@ -16,7 +16,7 @@ describe("FESK real time with known samples", () => {
 
     cy.get(".debug-panel summary", { timeout: 10000 }).click();
     cy.get("#sample2Btn").click();
-    cy.get("#out .decoded-ok", { timeout: 30000 })
+    cy.get("#out .decoded-ok", { timeout: 40000 })
       .should("not.be.empty")
       .should("contain.text", "test");
   });
