94b5821770a2130a00f4f43de11d3cad1bb753bd
Eirik S. Morland <eirik@morland.no>
2026-01-01 22:50:40 +0100

    Add a decode buffer to be sure

--- cypress/e2e/mic.cy.js
+++ cypress/e2e/mic.cy.js
@@ -3,6 +3,7 @@
 describe("FESK real time Test", () => {
   it("should decode audio when recording with fake microphone stream", () => {
     const recordDurationMs = Number(Cypress.env("recordDurationMs")) || 16000;
+    const decodeBufferMs = 3000;
     const expectedText = Cypress.env("expectedText") || "test";
     const modulation = Cypress.env("modulation") || "";
 
@@ -12,7 +13,7 @@ describe("FESK real time Test", () => {
     cy.get("#startBtn", { timeout: 10000 }).click();
     // At this point should auto-stop, since we passed the CRC.
     cy.get(".result-row .out-row-text.decoded-ok", {
-      timeout: recordDurationMs,
+      timeout: recordDurationMs + decodeBufferMs,
     }).should(($el) => {
       const text = $el.text().trim();
       const expected = expectedText.trim();
